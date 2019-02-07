ad7a27e588b0d3252b413f1d7a92c11bf9394e6e
JDchauhan <JDchauhan@github.com>
2019-02-07 21:15:45 +0530

    setting device on or off

---
+++ IotBoard/powerPi.py
@@ -0,0 +1,61 @@
+import machine
+import json
+
+d0 = 16 # wake
+d1 = 5
+d2 = 4
+d3 = 0 #flash
+d4 = 2
+d5 = 14
+d6 = 12
+d7 = 13
+d8 = 15
+d9 = 3
+d10 = 1
+
+
+pins = [
+    machine.Pin(d1, machine.Pin.OUT), 
+    machine.Pin(d2, machine.Pin.OUT), 
+    machine.Pin(d3, machine.Pin.OUT),
+    machine.Pin(d4, machine.Pin.OUT), 
+    machine.Pin(d5, machine.Pin.OUT), 
+    machine.Pin(d6, machine.Pin.OUT), 
+    machine.Pin(d7, machine.Pin.OUT), 
+    machine.Pin(d8, machine.Pin.OUT), 
+    machine.Pin(d9, machine.Pin.OUT), 
+    machine.Pin(d10, machine.Pin.OUT)
+]
+
+for pin in pins:
+    pin.low()
+
+with open("switches.json", "r") as jsonFile:
+    data = json.load(jsonFile)
+    for i in range (0,10):
+        if (data[str(i)] == 0):
+            pins[i].low()
+        else:
+            pins[i].high()
+
+
+def operate(num, msg):
+    with open("automation.json", "r") as jsonFile:
+        data = json.load(jsonFile)
+
+    if(msg == 'ON'):
+        data[str(num)] = 1
+        pins[num].high()
+
+    if(msg == 'OFF'):
+        pins[num].low()
+        data[str(num)] = 0
+
+    data = json.dumps(data)
+
+    print(data)
+    f = open('switches.json', "w")
+    f.write(data)
+    f.close()
+
+    print(num, msg)
